74990cd75a4afcb2823a73bf81a93430dcfed074
fahmi <kontak@fahmi.id>
2016-05-17 18:41:21 +0700

    Initialize: Provides generic databinding to any client library.

---
+++ Src/System.Data.Bindings/Src/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * 
+ * GTK-DATABIND
+ * Copyright (c) 2016 Sentrasoft (http://www.sentrasoft.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * Authored by: Fahmi <kontak@fahmi.id>
+ * 
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("System.Data.Bindings")]
+[assembly: AssemblyDescription("Provides generic databinding to any client library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sentrasoft")]
+[assembly: AssemblyProduct("System.Data.Bindings")]
+[assembly: AssemblyCopyright("GNU General Public License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// If the build and revision are set to '*' they will be updated automatically.
+
+[assembly: AssemblyVersion("4.5")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+[assembly: AssemblyDelaySign(false)]
+// [assembly: AssemblyKeyFile("mm.key")]
