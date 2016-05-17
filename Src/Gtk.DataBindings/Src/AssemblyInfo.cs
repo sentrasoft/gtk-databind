8bb59235091a9decbaa5c4be5752153c9c5c8268
fahmi <kontak@fahmi.id>
2016-05-17 18:52:32 +0700

    Clone from gtk-databind-lib.

---
+++ Src/Gtk.DataBindings/Src/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 
+ * GTK-DATABIND
+ * Copyright (c) 2016 Sentrasoft (http://www.sentrasoft.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * Authored by: Fahmi <kontak@fahmi.id>
+ * 
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following
+// attributes.
+//
+// change them to the information which is associated with the assembly
+// you compile.
+
+[assembly: AssemblyTitle("gtk-databind")]
+[assembly: AssemblyDescription("Present and interact with data.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sentrasoft")]
+[assembly: AssemblyProduct("gtk-databind")]
+[assembly: AssemblyCopyright("GNU General Public License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("neutral")]
+
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all values by your own or you can build default build and revision
+// numbers with the '*' character (the default):
+
+[assembly: AssemblyVersion("4.5")]
+
+// The following attributes specify the key for the sign of your assembly. See the
+// .NET Framework documentation for more information about signing.
+// This is not required, if you don't want signing let these attributes like they're.
+[assembly: AssemblyDelaySign(false)]
+// [assembly: AssemblyKeyFile("mm.key")]
